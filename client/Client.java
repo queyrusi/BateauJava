2cabf82326b1881a7e4fb3141a017a7a1fcb192b
Simon Queyrut <38203266+queyrusi@users.noreply.github.com>
2019-02-15 00:21:52 +0100

    Add files via upload
    Début squelette avec pattern Etat implémenté, début d'implémentation Observer/observable + un peu de javadoc

---
+++ client/Client.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package client;
+
+/**
+ * @author chenqun
+ *
+ */
+public abstract class Client {
+
+}
---
+++ client/User.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package client;
+
+/**
+ * Discuter avec le serveur
+ * 
+ * Collaborateurs :
+ * 
+ * +--------------+    +-------------+    
+ * |  IHMClient   |    | ServeurTCP  |    
+ * +--------------+    +-------------+    
+ * |              |    |             |    
+ * |              |    |             |    
+ * +--------------+    +-------------+   
+ * 
+ * # ascii art was generated on asciiflow.com/
+ * 
+ * @author chenqun
+ *
+ */
+public class User extends Client {
+
+}
