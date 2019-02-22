08dd87514e9a260170c60312584543ac9e0d1adf
CaineSilva <47333696+CaineSilva@users.noreply.github.com>
2019-02-22 15:18:55 +0100

    Add files via upload

--- server/MainServeur.java
+++ server/MainServeur.java
@@ -7,7 +7,7 @@ public class MainServeur {
 	public static void main(String[] args) {
 		CentralContext context = new CentralContext();
 		ConnectionProtocol protocol = new ConnectionProtocol();
-		ServeurTCP serveur = new ServeurTCP(6666);
+		ServeurTCP serveur = new ServeurTCP(context, protocol, 6666);
 		serveur.start();
 	}
 }
