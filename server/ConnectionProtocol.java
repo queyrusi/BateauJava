e2e324d498605135be9dcd048ee82b744b6febe7
CaineSilva <47333696+CaineSilva@users.noreply.github.com>
2019-02-25 15:36:23 +0100

    Add files via upload

--- server/ConnectionProtocol.java
+++ server/ConnectionProtocol.java
@@ -13,12 +13,29 @@ import server.patterns.tcp.IProtocole;
 import server.requestHandlers.MonitoredStateRequestHandler;
 import server.requestHandlers.UserRequestHandler;
 
+/**
+ * <strong>Description : </strong>Classe implémentant IProtocole. Le thread se charge d'écouter le socket pour valider la connexion d'un utilisateur ou d'un bateau. 
+ * Il lance le gestionnaire associé.
+ * @author C.Silva, R.Cuinat
+ * 
+ */
 public class ConnectionProtocol implements IProtocole {
-
+	/**
+	 * <strong>Description : </strong>Classe implémentant IProtocole. Le thread se charge d'écouter le socket pour valider la connexion d'un utilisateur ou d'un bateau. 
+	 * Il lance le gestionnaire associé.
+	 * @author C.Silva, R.Cuinat
+	 * 
+	 */
 	public ConnectionProtocol() {
-		// TODO Auto-generated constructor stub
 	}
-
+	
+	/**
+	 * <strong>Description : </strong>Lis la première ligne du InputStream et vérifie la validité du code ('@User' ou '@Ship'). Lance le gestionnaire associé
+	 * @author C.Silva, R.Cuinat
+	 * @param aContext Contexte du serveur. Implémente l'interface IContext
+	 * @param anInputStream Stream d'entrée de la connexion
+	 * @param anOutputStream Stream de sortie de la connexion
+	 */
 	@Override
 	public void execute(IContext aContext, InputStream anInputStream, OutputStream anOutputStream) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
@@ -29,10 +46,12 @@ public class ConnectionProtocol implements IProtocole {
 			switch (connectionCode) {
 			case "@Ship" :
 				MonitoredStateRequestHandler MSt = new MonitoredStateRequestHandler(aContext, socketIn, socketOut);
+				System.out.println("Connexion d'un Bateau");
 				MSt.start();
 				break;
 			case "@User" :
 				UserRequestHandler URt = new UserRequestHandler(aContext, socketIn, socketOut);
+				System.out.println("Connexion d'un Utilisateur");
 				URt.start();
 				break;
 			default :
