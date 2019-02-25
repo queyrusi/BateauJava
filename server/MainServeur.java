6cfc5598ee50a39e7135b6bfeee85980eb25ab53
CaineSilva <47333696+CaineSilva@users.noreply.github.com>
2019-02-25 14:33:45 +0100

    Add files via upload

--- server/MainServeur.java
+++ server/MainServeur.java
@@ -1,11 +1,20 @@
 package server;
 
+import server.entities.Ship;
+import server.entities.User;
 import server.patterns.tcp.ServeurTCP;
 
 public class MainServeur {
 
 	public static void main(String[] args) {
 		CentralContext context = new CentralContext();
+		try {
+			context.addUser("toto", new User("toto", "", "", null, null));
+			context.addShip("tata", new Ship("tata", "", "", ""));
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ConnectionProtocol protocol = new ConnectionProtocol();
 		ServeurTCP serveur = new ServeurTCP(context, protocol, 6666);
 		serveur.start();
