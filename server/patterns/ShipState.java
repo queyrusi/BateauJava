1569e5b21a508d4c7b6692ba7ea6983a32e6f46b
CaineSilva <47333696+CaineSilva@users.noreply.github.com>
2019-02-25 14:58:48 +0100

    Add files via upload

--- server/patterns/ShipState.java
+++ server/patterns/ShipState.java
@@ -3,20 +3,26 @@ package server.patterns;
 import server.entities.Ship;
 import server.patterns.tcp.RequestHandler;
 
+/**
+ * <strong>Description : </strong>Etat abstrait d'un bateau
+ * @author C.Silva, R.Cuinat
+ */
 public abstract class ShipState {
 	private Ship ship;
-	private RequestHandler thread;
-	
+	/**
+	 * <strong>Description : </strong>Constructeur de l'état
+	 * @author C.Silva, R.Cuinat
+	 * @param ship Betau associé à l'état
+	 */
 	public ShipState(Ship ship) {
 		// TODO Auto-generated constructor stub
 		this.ship = ship;
 	}
-	
+	/**
+	 * <strong>Description : </strong>Getter du bateau associé à l'état
+	 * @author C.Silva, R.Cuinat
+	 */
 	public Ship getShip() {
 		return ship;
-	}
-	
-	public RequestHandler getThread() {
-		return thread;
 	}
 }
