fd06ed7ee55e25aaf19a7de6ea030e1c5fe4d367
Simon Queyrut <38203266+queyrusi@users.noreply.github.com>
2019-02-25 16:23:38 +0100

    Create ClientTest.java

---
+++ client/tests/ClientTest.java
@@ -0,0 +1,37 @@
+package client.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import client.Monitoring;
+import client.NoMonitoring;
+import client.Stolen;
+import client.SystemeEmbarque;
+import client.Tracking;
+import client.User;
+
+class ClientTest {
+
+	@Test
+	public void clientMethods() {
+		
+		User userTest = new User("nomTest", 6666, "toto");
+		assertEquals(userTest.getLogin(), "toto");
+		assertEquals(userTest.getNomServeur(), "nomTest");
+		assertEquals(userTest.getNumeroPort(), 6666);
+		assertEquals(userTest.getTypeConnexion(), "@User");
+		
+	}
+	
+	@Test
+	public void sysEmbarqueState() {
+		
+		SystemeEmbarque sysEmbTest = new SystemeEmbarque("nomTest", 6666, "tata");
+		assertEquals(sysEmbTest.getMonitoringState().getClass(), Monitoring.class);
+		assertEquals(sysEmbTest.getNoMonitoringState().getClass(), NoMonitoring.class);
+		assertEquals(sysEmbTest.getTrackingState().getClass(), Tracking.class);
+		assertEquals(sysEmbTest.getStolenState().getClass(), Stolen.class);
+		
+	}
+
+}
