5c755c4b90178da3adda890f6b10a1e2712cbc7f
CaineSilva <47333696+CaineSilva@users.noreply.github.com>
2019-02-25 14:37:04 +0100

    Add files via upload

--- server/patterns/io/ILogger.java
+++ server/patterns/io/ILogger.java
@@ -1,9 +1,17 @@
 package server.patterns.io;
 
 import java.io.OutputStream;
-
+/**
+ * <strong>Description : </strong> Interface pour logger des informations
+ * @author C.Silva, R.Cuinat
+ */
 public interface ILogger {
-	
+	/**
+	 * <strong>Description : </strong>Ecrit une ligne dans le flux de sortie
+	 * @author C.Silva, R.Cuinat
+	 * @param line ligne à écrire
+	 * @param oStream flux d'écriture
+	 */
 	public void writeLog(String line, OutputStream oStream);
 
 }
--- server/patterns/io/IReader.java
+++ server/patterns/io/IReader.java
@@ -1,9 +1,17 @@
 package server.patterns.io;
 
 import java.io.InputStream;
-
+/**
+ * <strong>Description : </strong>Interface pour lire des informations
+ * @author C.Silva, R.Cuinat
+ */
 public interface IReader {
-	
-	public void readLog(InputStream iStream);
+	/**
+	 * <strong>Description : </strong>Lis la première ligne du InputStream et vérifie la validité du code ('@User' ou '@Ship'). Lance le gestionnaire associé
+	 * @author C.Silva, R.Cuinat
+	 * @param iStream Stream de lecture
+	 * @return ligne lue
+	 */
+	public String readLog(InputStream iStream);
 
 }
