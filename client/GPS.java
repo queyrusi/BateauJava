b8a591bae8ab993352d00b029d2d0c236d9deae5
Simon Queyrut <38203266+queyrusi@users.noreply.github.com>
2019-03-05 00:22:38 +0100

    Create GPS.java
    Classe GPS qui hérite de Capteur

---
+++ client/GPS.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package client;
+
+
+
+/**
+ * @author chenqun
+ *
+ */
+public class GPS extends Capteur {
+
+	/**
+	 * @param newCapteurName
+	 * @param newCapteurLabel
+	 */
+	public GPS(String newCapteurName, String newCapteurLabel) {
+		super(newCapteurName, newCapteurLabel);
+		// TODO pas le bon argument
+	}
+
+}
