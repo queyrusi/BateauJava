3e50a81b2fdd29010a0d7eb35ec4d910b47dda29
Simon Queyrut <38203266+queyrusi@users.noreply.github.com>
2019-03-12 18:10:22 +0100

    Add files via upload
    Javadoc ftw

--- client/SystAlarme.java
+++ client/SystAlarme.java
@@ -22,7 +22,7 @@ import java.util.Observer;
  * d'alarme.</dd>
  * </dl>
  * 
- * @author chenqun
+ * @author P. Lledo, S. Queyrut
  *
  */
 @SuppressWarnings("deprecation")
@@ -35,19 +35,38 @@ public class SystAlarme extends Observable implements Observer {  // observable 
 	
 	boolean isTriggered = false;  // alarme non déclenchée
 	
-	
+	/**
+	 * <strong>Description : </strong> Constructeur pour le système d'alarme.
+	 * 
+	 * @param newSystemeEmbarque - système embarqué auquel appartient le système d'alarme.
+	 * @author P. Lledo, S. Queyrut
+	 */
 	public SystAlarme(SystemeEmbarque newSystemeEmbarque) {
 		
 		this.addObserverPerso(newSystemeEmbarque);
 		
 	}
 
+	/**
+	 * <strong>Description : </strong> Ajoute un abonné au système d'alarme.
+	 * 
+	 * @param newSystemeEmbarque - système embarqué auquel appartient le système d'alarme.
+	 * @author P. Lledo, S. Queyrut
+	 */
 	private void addObserverPerso(SystemeEmbarque newSystemeEmbarque) {
 
 		addObserver(newSystemeEmbarque);
 		this.systemesEmbList.add(newSystemeEmbarque);
 	}
 
+	/**
+	 * <strong>Description : </strong> Surcharge de la méthode {@code update}, met le système d'alarme en branle s'il reçoit
+	 * un message d'alarme, notifie le système embarqué de cette alarme.
+	 * 
+	 * @param o - observable.
+	 * @param arg - String contenant un message d'alarme.
+	 * @author P. Lledo, S. Queyrut
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
 	
